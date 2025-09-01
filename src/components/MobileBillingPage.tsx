9b3b6683ab7da58dae9b76e212ab0691f4d269ed
margesh64 <105532336+margesh64@users.noreply.github.com>
2025-09-01 21:05:45 +0530

    Updated JamaReceiptTemplate_11zon.jpg

--- src/components/MobileBillingPage.tsx
+++ src/components/MobileBillingPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { AdvancedBillManagement } from './billing/AdvancedBillManagement';
+import { GujaratiBillManagement } from './billing/GujaratiBillManagement';
 
 export function MobileBillingPage() {
-  return <AdvancedBillManagement />;
+  return <GujaratiBillManagement />;
 }
